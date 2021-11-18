df4bd86c61783e9cbb01e983cd10fb50dc087336
eis-group-opensource <eis-group-opensource@eisgroup.com>
2021-11-18 16:29:42 +0200

    Repository sync on 18-11-2021_16:29

---
+++ src/test/java/org/codehaus/mojo/javacc/MainStub.java
@@ -0,0 +1,45 @@
+/* Copyright © 2016 EIS Group and/or one of its affiliates. All rights reserved. Unpublished work under U.S. copyright laws.
+ CONFIDENTIAL AND TRADE SECRET INFORMATION. No portion of this work may be copied, distributed, modified, or incorporated into any other media without EIS Group prior written consent.*/
+package org.codehaus.mojo.javacc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file 
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied.  See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ */
+
+/**
+ * Supports {@link ForkedJvmTest}.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id: MainStub.java 6212 2008-02-05 19:34:03Z bentmann $
+ */
+public class MainStub
+{
+
+    public static void main( String[] args )
+    {
+        for ( int i = 0; i < args.length; i++ )
+        {
+            System.out.println( args[i] );
+        }
+
+        System.err.println( System.getProperty( "user.dir" ) );
+
+        System.exit( 27 );
+    }
+
+}
